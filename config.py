7f695966d6c01db8c0c4ccf049105f5b811ae6c0
utkarsh764 <utkarshsrivastavalala123@gmail.com>
2025-03-13 21:46:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 # Required Variables Config
 API_ID = int(os.environ.get("API_ID", "26926782"))
 API_HASH = os.environ.get("API_HASH", "9b2fac908fb7f9a3dabac3b0a57211b1")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7786105206:AAHm0iceXktLQugF_5xs-lRj8xjbtMR-xE0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8184665595:AAHasatOstcUgATCWbneK-j95ojTpmr53vo")
 ADMIN = int(os.environ.get("ADMIN", "1214167849"))
 
 
@@ -19,7 +19,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002354465117"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Utkarsh123:9335924360@utkarsh9.af91n.mongodb.net/?retryWrites=true&w=majority&appName=Utkarsh9")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Utkarsh123:<db_password>@utkarsh9.af91n.mongodb.net/?retryWrites=true&w=majority&appName=Utkarsh9")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Utkarsh9")
 
 
